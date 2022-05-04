10e7a2900d2504e32ada547932193a99a86aa8ba
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-05-04 12:54:46 +0430

    write first celery code

---
+++ Day 57 - Celery/tasks.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+app = Celery('tasks',
+             broker='redis://localhost:6379/',
+             backend="redis://localhost:6379",
+             )
+
+
+@app.task
+def add(x, y):
+    return x + y
